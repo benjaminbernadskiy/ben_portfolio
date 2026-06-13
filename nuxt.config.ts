8754d7d319765a1a0e544246d18bfa64b9d6c84b
Benjamin Bernadskiy <bbernadskiy1224@gmail.com>
2026-06-13 17:47:45 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,15 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
   css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico?v=20260613",
+        },
+      ],
+    },
+  },
 });
